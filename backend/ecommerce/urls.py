2f03a1eb04ad5d667f44e4a616e3c7c9be6933a8
Mohamed Younis <75328954+younissm@users.noreply.github.com>
2026-08-09 17:15:38 +0300

    Add backend core configuration: requirements, manage.py, Django settings, URLs, WSGI, ASGI

---
+++ backend/ecommerce/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import os
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/users/", include('accounts.urls')),
+    path('api/', include('store.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
